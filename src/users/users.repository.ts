86371ee37590cca1a7b4f6fc30ca46e3abbd7544
Thiago Moraes <thiago4x70@gmail.com>
2021-08-07 15:34:17 -0300

    minor fix in the users repository dependency import path

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,8 +1,8 @@
 import { User, UserDocument } from './schemas/user.schema';
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { FilterQuery, Model } from 'mongoose';
-import { EntityRepository } from 'src/database/entity.repository';
+import { Model } from 'mongoose';
+import { EntityRepository } from '../database/entity.repository';
 
 @Injectable()
 export class UsersRepository extends EntityRepository<UserDocument> {
